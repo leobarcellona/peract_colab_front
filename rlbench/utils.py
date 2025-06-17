9509d54121b5961ca08f3398262840b485bc0e7b
leobarcellona <50733674+leobarcellona@users.noreply.github.com>
2025-06-17 11:58:41 +0200

    Update utils.py

--- rlbench/utils.py
+++ rlbench/utils.py
@@ -12,10 +12,7 @@ from pyrep.objects import VisionSensor
 EPISODE_FOLDER = 'episode%d'
 
 CAMERA_FRONT = 'front'
-CAMERA_LS = 'left_shoulder'
-CAMERA_RS = 'right_shoulder'
-CAMERA_WRIST = 'wrist'
-CAMERAS = [CAMERA_FRONT, CAMERA_LS, CAMERA_RS, CAMERA_WRIST]
+CAMERAS = [CAMERA_FRONT]
 
 IMAGE_RGB = 'rgb'
 IMAGE_DEPTH = 'depth'
@@ -41,41 +38,13 @@ def get_stored_demo(data_path, index):
   num_steps = len(obs)
   for i in range(num_steps):
     obs[i].front_rgb = np.array(Image.open(os.path.join(episode_path, '%s_%s' % (CAMERA_FRONT, IMAGE_RGB), IMAGE_FORMAT % i)))
-    obs[i].left_shoulder_rgb = np.array(Image.open(os.path.join(episode_path, '%s_%s' % (CAMERA_LS, IMAGE_RGB), IMAGE_FORMAT % i)))
-    obs[i].right_shoulder_rgb = np.array(Image.open(os.path.join(episode_path, '%s_%s' % (CAMERA_RS, IMAGE_RGB), IMAGE_FORMAT % i)))
-    obs[i].wrist_rgb = np.array(Image.open(os.path.join(episode_path, '%s_%s' % (CAMERA_WRIST, IMAGE_RGB), IMAGE_FORMAT % i)))
-
+  
     obs[i].front_depth = image_to_float_array(Image.open(os.path.join(episode_path, '%s_%s' % (CAMERA_FRONT, IMAGE_DEPTH), IMAGE_FORMAT % i)), DEPTH_SCALE)
     near = obs[i].misc['%s_camera_near' % (CAMERA_FRONT)]
     far = obs[i].misc['%s_camera_far' % (CAMERA_FRONT)]
     obs[i].front_depth = near + obs[i].front_depth * (far - near)
 
-    obs[i].left_shoulder_depth = image_to_float_array(Image.open(os.path.join(episode_path, '%s_%s' % (CAMERA_LS, IMAGE_DEPTH), IMAGE_FORMAT % i)), DEPTH_SCALE)
-    near = obs[i].misc['%s_camera_near' % (CAMERA_LS)]
-    far = obs[i].misc['%s_camera_far' % (CAMERA_LS)]
-    obs[i].left_shoulder_depth = near + obs[i].left_shoulder_depth * (far - near)
-
-    obs[i].right_shoulder_depth = image_to_float_array(Image.open(os.path.join(episode_path, '%s_%s' % (CAMERA_RS, IMAGE_DEPTH), IMAGE_FORMAT % i)), DEPTH_SCALE)
-    near = obs[i].misc['%s_camera_near' % (CAMERA_RS)]
-    far = obs[i].misc['%s_camera_far' % (CAMERA_RS)]
-    obs[i].right_shoulder_depth = near + obs[i].right_shoulder_depth * (far - near)
-
-    obs[i].wrist_depth = image_to_float_array(Image.open(os.path.join(episode_path, '%s_%s' % (CAMERA_WRIST, IMAGE_DEPTH), IMAGE_FORMAT % i)), DEPTH_SCALE)
-    near = obs[i].misc['%s_camera_near' % (CAMERA_WRIST)]
-    far = obs[i].misc['%s_camera_far' % (CAMERA_WRIST)]
-    obs[i].wrist_depth = near + obs[i].wrist_depth * (far - near)
-
     obs[i].front_point_cloud = VisionSensor.pointcloud_from_depth_and_camera_params(obs[i].front_depth, 
                                                                                     obs[i].misc['front_camera_extrinsics'],
                                                                                     obs[i].misc['front_camera_intrinsics'])
-    obs[i].left_shoulder_point_cloud = VisionSensor.pointcloud_from_depth_and_camera_params(obs[i].left_shoulder_depth, 
-                                                                                            obs[i].misc['left_shoulder_camera_extrinsics'],
-                                                                                            obs[i].misc['left_shoulder_camera_intrinsics'])
-    obs[i].right_shoulder_point_cloud = VisionSensor.pointcloud_from_depth_and_camera_params(obs[i].right_shoulder_depth, 
-                                                                                             obs[i].misc['right_shoulder_camera_extrinsics'],
-                                                                                             obs[i].misc['right_shoulder_camera_intrinsics'])
-    obs[i].wrist_point_cloud = VisionSensor.pointcloud_from_depth_and_camera_params(obs[i].wrist_depth, 
-                                                                                           obs[i].misc['wrist_camera_extrinsics'],
-                                                                                           obs[i].misc['wrist_camera_intrinsics'])
-    
   return obs
