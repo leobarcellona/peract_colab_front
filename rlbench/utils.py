30de5cc7ba2d8a2f758eb4dc667d135bb7dfc138
leobarcellona <50733674+leobarcellona@users.noreply.github.com>
2025-06-20 10:01:49 +0200

    Update utils.py

--- rlbench/utils.py
+++ rlbench/utils.py
@@ -24,7 +24,7 @@ VARIATION_NUMBER_PICKLE = 'variation_number.pkl'
 DEPTH_SCALE = 2**24 - 1
 
 # functions
-def get_stored_demo(data_path, index):
+def get_stored_demo(data_path, index, cameras):
   episode_path = os.path.join(data_path, EPISODE_FOLDER % index)
   
   # low dim pickle file
@@ -37,14 +37,27 @@ def get_stored_demo(data_path, index):
 
   num_steps = len(obs)
   for i in range(num_steps):
-    obs[i].front_rgb = np.array(Image.open(os.path.join(episode_path, '%s_%s' % (CAMERA_FRONT, IMAGE_RGB), IMAGE_FORMAT % i)))
-  
-    obs[i].front_depth = image_to_float_array(Image.open(os.path.join(episode_path, '%s_%s' % (CAMERA_FRONT, IMAGE_DEPTH), IMAGE_FORMAT % i)), DEPTH_SCALE)
-    near = obs[i].misc['%s_camera_near' % (CAMERA_FRONT)]
-    far = obs[i].misc['%s_camera_far' % (CAMERA_FRONT)]
-    obs[i].front_depth = near + obs[i].front_depth * (far - near)
+    for camera in cameras:
 
-    obs[i].front_point_cloud = VisionSensor.pointcloud_from_depth_and_camera_params(obs[i].front_depth, 
-                                                                                    obs[i].misc['front_camera_extrinsics'],
-                                                                                    obs[i].misc['front_camera_intrinsics'])
+      near = obs[i].misc['%s_camera_near' % (camera)]
+      far = obs[i].misc['%s_camera_far' % (camera)]
+
+      decompressed_depth = image_to_float_array(Image.open(os.path.join(episode_path, '%s_%s' % (camera, IMAGE_DEPTH), IMAGE_FORMAT % i)), DEPTH_SCALE)
+      decompressed_depth = near + decompressed_depth * (far - near)
+      point_cloud = VisionSensor.pointcloud_from_depth_and_camera_params(decompressed_depth,
+                                                                         obs[i].misc['%s_camera_extrinsics' % camera],
+                                                                         obs[i].misc['%s_camera_intrinsics' % camera])
+
+      setattr(obs[i], '%s_rgb' % camera,  np.array(Image.open(os.path.join(episode_path, '%s_%s' % (camera, IMAGE_RGB), IMAGE_FORMAT % i))))
+      setattr(obs[i], '%s_depth' % camera, decompressed_depth)
+      setattr(obs[i], '%s_point_cloud' % camera, point_cloud)
+
+
+      #obs[i].front_rgb = np.array(Image.open(os.path.join(episode_path, '%s_%s' % (camera, IMAGE_RGB), IMAGE_FORMAT % i)))
+      #obs[i].front_depth = image_to_float_array(Image.open(os.path.join(episode_path, '%s_%s' % (camera, IMAGE_DEPTH), IMAGE_FORMAT % i)), DEPTH_SCALE)
+      #obs[i].front_depth = near + obs[i].front_depth * (far - near)
+      #obs[i].front_point_cloud = VisionSensor.pointcloud_from_depth_and_camera_params(obs[i].front_depth,
+      #                                                                                obs[i].misc['%s_camera_extrinsics' % camera],
+      #                                                                                obs[i].misc['%s_camera_intrinsics' % camera])
+
   return obs
